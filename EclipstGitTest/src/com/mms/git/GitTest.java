8659db0975b472e41bf9b6d45def986a6ac68339
Raji Komatreddy <rkomatreddy@webmd.net>
2016-10-18 21:42:54 -0400

    modified by neelima

--- EclipstGitTest/src/com/mms/git/GitTest.java
+++ EclipstGitTest/src/com/mms/git/GitTest.java
@@ -3,7 +3,7 @@ package com.mms.git;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("this is to test eclipse and git integration");
+		System.out.println("this is modified by Neelima");
 
 	}
 
